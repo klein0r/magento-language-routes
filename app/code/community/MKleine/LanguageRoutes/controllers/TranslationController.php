c10c838d956d4b97c3204a6edc07c2dd4e587526
Matthias Kleine <matthias@klein0r.de>
2014-11-08 16:16:52 +0100

    Inline translation block

--- app/code/community/MKleine/LanguageRoutes/controllers/TranslationController.php
+++ app/code/community/MKleine/LanguageRoutes/controllers/TranslationController.php
@@ -13,6 +13,10 @@ class MKleine_LanguageRoutes_TranslationController extends Mage_Core_Controller_
             $returlUrl = $this->getRequest()->getParam('current_url', false);
             $storeId = $this->getRequest()->getParam('store_id', false);
 
+            $origRoute = $this->getRequest()->getParam('route_untranslated', false);
+            $origController = $this->getRequest()->getParam('controller_untranslated', false);
+            $origAction = $this->getRequest()->getParam('action_untranslated', false);
+
             $newRoute = $this->getRequest()->getParam('route', false);
             $newController = $this->getRequest()->getParam('controller', false);
             $newAction = $this->getRequest()->getParam('action', false);
@@ -22,21 +26,21 @@ class MKleine_LanguageRoutes_TranslationController extends Mage_Core_Controller_
                     'type_id' => MKleine_LanguageRoutes_Model_Languageroute::LANGUAGEROUTE_TYPE_ROUTER,
                     'store_id' => $storeId,
                     'is_active' => 1,
-                    'value' => $this->getRequest()->getParam('route_untranslated', false),
+                    'value' => $origRoute,
                     'translation' => $newRoute
                 ),
                 array(
                     'type_id' => MKleine_LanguageRoutes_Model_Languageroute::LANGUAGEROUTE_TYPE_CONTROLLER,
                     'store_id' => $storeId,
                     'is_active' => 1,
-                    'value' => $this->getRequest()->getParam('controller_untranslated', false),
+                    'value' => $origController,
                     'translation' => $newController
                 ),
                 array(
                     'type_id' => MKleine_LanguageRoutes_Model_Languageroute::LANGUAGEROUTE_TYPE_ACTION,
                     'store_id' => $storeId,
                     'is_active' => 1,
-                    'value' => $this->getRequest()->getParam('action_untranslated', false),
+                    'value' => $origAction,
                     'translation' => $newAction
                 )
             );
@@ -74,7 +78,11 @@ class MKleine_LanguageRoutes_TranslationController extends Mage_Core_Controller_
             Mage::helper('mk_languageroutes')->clearTranslationCache();
 
             if ($returlUrl) {
-                $this->_redirect(sprintf('%s/%s/%s', $newRoute, $newController, $newAction));
+                $this->_redirect(sprintf('%s/%s/%s',
+                    $newRoute ?: $origRoute,
+                    $newController ?: $origController,
+                    $newAction ?: $origAction
+                ));
             }
             else {
                 $this->_redirect('*/*/index');
