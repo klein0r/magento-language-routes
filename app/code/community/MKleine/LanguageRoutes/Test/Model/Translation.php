46e26a94c998fceef4f6a0136e59894d9bc89369
Matthias Kleine <matthias@klein0r.de>
2014-11-16 21:22:15 +0100

    Travis CI test

---
+++ app/code/community/MKleine/LanguageRoutes/Test/Model/Translation.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright (c) 2014 Matthias Kleine
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@mkleine.de so we can send you a copy immediately.
+ *
+ * @category    MKleine
+ * @package     MKleine_LanguageRoutes
+ * @copyright   Copyright (c) 2014 Matthias Kleine (http://mkleine.de)
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+class MKleine_LanguageRoutes_Test_Model_Translation
+    extends EcomDev_PHPUnit_Test_Case
+{
+
+}
